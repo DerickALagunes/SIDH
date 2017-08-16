a22440e93ff0b2538db88264a4db51025356c641
narock <narock@oswaldo>
2017-08-16 17:52:29 -0500

    DEBUG MAIL

--- src/java/mx/edu/utez/sidh/utils/EnviarCorreo.java
+++ src/java/mx/edu/utez/sidh/utils/EnviarCorreo.java
@@ -17,10 +17,8 @@ import mx.edu.utez.sidh.bean.Usuario;
 public class EnviarCorreo {
 
     public void enviarA(Usuario usuario) {
-
         final String username = "sistema.sidh@gmail.com";
         final String password = "sistemasidh2017";
-
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
@@ -33,21 +31,28 @@ public class EnviarCorreo {
                 return new PasswordAuthentication(username, password);
             }
         });
+        System.out.println("Enviar correo B9 ----");
 
         try {
-
+            System.out.println("Enviar correo B10 ----");
             Message message = new MimeMessage(session);
+            System.out.println("Enviar correo B11 ----");
             message.setFrom(new InternetAddress("sistema.sidh@gmail.com"));
+            System.out.println("Enviar correo B12 ----");
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(usuario.getEmail()));
+            System.out.println("Enviar correo B13 ----");
             message.setSubject("Recordatorio, llena tu disponibilidad para este periodo!");
+            System.out.println("Enviar correo B14 ----");
             message.setText("Buen día "+usuario.getNombre()+" "+usuario.getApellidoPaterno()+","
                     + "\n\n Le recordamos que aun no ha indicado su disponibilidad para el nuevo cuatrimestre");
+            System.out.println("Enviar correo B15 ----");
 
             Transport.send(message);
+            System.out.println("Enviar correo B16 ----");
 
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
